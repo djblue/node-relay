48806535671d7daeff9703bb8b66fcff65eb24ff
Chris Badahdah <a.s.badahdah@gmail.com>
2015-09-13 20:57:14 -0700

    Fixed `relay start` to use token.

--- cli/start.js
+++ cli/start.js
@@ -7,6 +7,7 @@ var usage = function () {
 exports.run = function (argv, done) {
   server.start({
     port: argv.port || process.env.PORT || 3000
+    token: argv.token || process.env.RELAY_TOKEN
   }).on('bind', function (port) {
     log.info('listening on port = ' + port);
   });
